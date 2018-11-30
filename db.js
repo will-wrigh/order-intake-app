96953a08f5fb62738e435ff7704345c268402529
Shalini Sreedhar <shalini.sreedhar@emory.edu>
2018-11-29 19:46:03 -0500

    updated export to csv functionality

--- db.js
+++ db.js
@@ -11,12 +11,12 @@ var con = mysql.createConnection({
 // var create = "CREATE TABLE queue (id INT AUTO_INCREMENT PRIMARY KEY, proj_name VARCHAR(255), full_name VARCHAR(255), phone_num INT, netID VARCHAR(255), association VARCHAR(255), color VARCHAR(255), printer VARCHAR(255), infill VARCHAR(255), resolution  VARCHAR(255), material INT, time_hr INT, time_min INT, file VARCHAR(255), purpose VARCHAR(255), date VARCHAR(255), completed VARCHAR(255) DEFAULT 'no', emailSent VARCHAR(255) DEFAULT 'no', paidFor VARCHAR(255) DEFAULT 'no', imageTaken VARCHAR(255) DEFAULT 'no')";
 // var add = "INSERT INTO queue (proj_name, full_name) VALUES ('Koala', 'Joe')";
  var sel = "SELECT * FROM queue"
- var add = "INSERT INTO queue (proj_name, full_name, phone_num, netID , association, color, printer, infill, resolution, material, time_min, file, purpose, date, comments) \
-                              VALUES ('','Kevin Kirsch', '','kdkirsc','Emory University','White','Form','5%','0.10',5,10,'slep.form','Personal','9/4/2018','')"
+ //var add = "INSERT INTO queue (proj_name, full_name, phone_num, netID , association, color, printer, infill, resolution, material, time_min, file, purpose, date, comments) \
+                              //VALUES ('','Kevin Kirsch', '','kdkirsc','Emory University','White','Form','5%','0.10',5,10,'slep.form','Personal','9/4/2018','')"
 // var alter = "ALTER TABLE queue ADD COLUMN comments VARCHAR(255)"
 // var del = "ALTER TABLE queue DROP COLUMN completed"
 // var addCol = "ALTER TABLE queue ADD COLUMN completed VARCHAR(255) DEFAULT 'no'"
-var change = "UPDATE queue SET time_min = 1413 WHERE id=106"
+//var change = "UPDATE queue SET time_min = 1413 WHERE id=106"
 //var delData = "DELETE FROM queue WHERE id =114 "
 con.connect(function(err) {
   if (err) throw err;
